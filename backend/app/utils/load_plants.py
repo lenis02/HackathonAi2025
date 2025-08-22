2309ddcae2a78ca3359b130b6e63633290f98992
lenis02 <2655362@naver.com>
2025-08-22 09:55:55 +0900

    delete js folders

--- backend/app/utils/load_plants.py
+++
@@ -1,7 +0,0 @@
-import json
-import os
-
-def load_plants():
-    plants_path = os.path.join(os.path.dirname(__file__), "plants.json")
-    with open(plants_path, "r", encoding="utf-8") as f:
-        return json.load(f)
